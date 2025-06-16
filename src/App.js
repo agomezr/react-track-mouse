4942bfbd68a28956a6afd694ac9defe1a12c45ef
Alberto <alberto@masweb.es>
2025-06-17 01:04:51 +0200

    Commit inicial

--- src/App.js
+++ src/App.js
@@ -1,23 +1,71 @@
-import logo from './logo.svg';
-import './App.css';
+import "./App.css";
+import { useEffect, useState } from "react";
+
+const withMousePosition = (WrappedComponent) => {
+
+  return( props => {
+
+    const [mousePosition, setMousePosition] = useState({ x: 0, y: 0 });
+
+    useEffect(() => {
+      const handleMousePositionChange = (e) => {
+        setMousePosition({
+          x: e.clientX,
+          y: e.clientY,
+        });
+      };
+
+      window.addEventListener("mousemove", handleMousePositionChange);
+
+      return () => {  //Destrucción del listener al destruir el componente
+        window.removeEventListener("mousemove", handleMousePositionChange);
+      };
+    }, []);
+
+    return <WrappedComponent {...props} mousePosition={mousePosition}/>;
+
+  })  
+};
+
+// This component should not receive any props
+const PanelMouseLogger = ({mousePosition}) => {
+  if (!mousePosition){
+    return null;
+  }
+
+  return (
+    <div className="BasicTracker">
+      <p>Mouse position:</p>
+      <div className="Row">
+        <span>x: {mousePosition.x}</span>
+        <span>y: {mousePosition.y}</span>
+      </div>
+    </div>
+  );
+};
+
+// This component should not receive any props
+const PointMouseLogger = ({mousePosition}) => {
+  // The below if statement can be removed after the render props pattern is implemented
+  if (!mousePosition) {
+    return null;
+  }
+  return (
+    <p>
+      ({mousePosition.x}, {mousePosition.y})
+    </p>
+  )
+};
+
+const PanelMouseTracker = withMousePosition(PanelMouseLogger);
+const PointMouseTracker = withMousePosition(PointMouseLogger);
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <header className="Header">Little Lemon Restaurant 🍕</header>
+      <PanelMouseTracker />
+      <PointMouseTracker />
     </div>
   );
 }
